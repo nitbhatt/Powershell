b9fd409ab79df18b034aa23b1f2fb38de9a6109c
nitbhatt <nitin.bhatt@microsoft.com>
2023-01-20 10:15:37 +0530

    Updating CreateAndApplyNewBackupPolicy.ps1

--- AzureRecoveryServicesVault/CreateAndApplyNewBackupPolicy.ps1
+++ AzureRecoveryServicesVault/CreateAndApplyNewBackupPolicy.ps1
@@ -1,20 +1,25 @@
-#Connect-azaccount
+Connect-azaccount
 $DailyRetention = 8 #days
 $WeeklyBackup= 4 #weeks
 $MonthlyBackup=3 #months
 $WorkloadType= 'AzureVM'
-$BackupPolicyName ='tp999rtttMast1'
+$BackupPolicyName ='test9996'
 
 $Subscriptions = Get-AzSubscription
+
 foreach ($sub in $Subscriptions) {
     Set-AzContext -SubscriptionName $sub
-    $rsvlist= Get-AzRecoveryServicesBackupProtectionPolicy | Select-Object Name
-          
-        If ($rsvlist -contains $BackupPolicyName) {
-            Write-Host "$BackPolicyName is already present in Subscription> $sub"    }
+    
+    $rsvs= Get-AzRecoveryServicesVault #| Select-Object Name
+    foreach ($rsv in $rsvs)  {
+            Get-AzRecoveryServicesVault -Name $rsv.Name | Set-AzRecoveryServicesVaultContext            
+            $rsvpollist= Get-AzRecoveryServicesBackupProtectionPolicy | Select-Object Name
+        
+            If ($rsvpollist -contains $BackupPolicyName) {
+            Write-Host "$BackPolicyName is already present in Subscription>>" $sub.Name    }
         
             else {
-                    Write-Host "`nCreating Policy>> $BackupPolicyName in Subscription==" $sub.Name 
+                    Write-Host "`nCreating Policy>>" $BackupPolicyName "Subscription>>" $sub.Name "Vault>>"$rsv.Name -ForegroundColor Green
                     $SchPol = Get-AzRecoveryServicesBackupSchedulePolicyObject -WorkloadType $WorkloadType
                     $SchPol.ScheduleRunTimes.Clear()
                     $timeZone = Get-TimeZone
@@ -28,33 +33,33 @@ foreach ($sub in $Subscriptions) {
                     #Getting a Base Backup Retention Policy object
                     $RetPol = Get-AzRecoveryServicesBackupRetentionPolicyObject -WorkloadType $WorkloadType -ScheduleRunFrequency  Daily
 
-                    Write-Host "`nSet Daily Schedule for $DailyRetention days..."
+                    Write-Host "`nSet Daily Schedule for $DailyRetention days..." -ForegroundColor Yellow
                     $RetPol.DailySchedule.DurationCountInDays = $DailyRetention
 
-                    Write-Host "Set Weekly Schedule for $WeeklyBackup weeks, every Saturday"
+                    Write-Host "Set Weekly Schedule for $WeeklyBackup weeks, every SATURDAY" -ForegroundColor Yellow
                     $RetPol.WeeklySchedule.DaysOfTheWeek = "Saturday"
                     $RetPol.WeeklySchedule.DurationCountInWeeks = $WeeklyBackup
 
 
-                    Write-Host "Set Monthly Schedule for $MonthlyBackup months..."
+                    Write-Host "Set Monthly Schedule for $MonthlyBackup months..." -ForegroundColor Yellow
                     $RetPol.MonthlySchedule.DurationCountInMonths = $MonthlyBackup
                     $RetPol.MonthlySchedule.RetentionScheduleFormatType= 'Daily'
-                    #$retpol.MonthlySchedule.RetentionScheduleDaily.DaysOfTheMonth= $MonthlyDays
                     $retpol.MonthlySchedule.RetentionScheduleDaily.DaysOfTheMonth[0].Date=0
                     $retpol.MonthlySchedule.RetentionScheduleDaily.DaysOfTheMonth[0].IsLast=$true
 
 
-                    Write-Host "No Yearly Schedule set"
+                    Write-Host "No Yearly Schedule set.." -ForegroundColor Red
                     $RetPol.IsYearlyScheduleEnabled= $false
 
                     #Setting Azure Recovery Services Vault Context
-                    Get-AzRecoveryServicesVault -Name $RSVaultName | Set-AzRecoveryServicesVaultContext
+                    #Get-AzRecoveryServicesVault -Name $RSVaultName | Set-AzRecoveryServicesVaultContext
 
                     $ProtectionPolicy = New-AzRecoveryServicesBackupProtectionPolicy -Name $BackupPolicyName `
                     -WorkloadType $WorkloadType -RetentionPolicy $RetPol -SchedulePolicy $SchPol
 
              }
          }
+        }
 
 
 
